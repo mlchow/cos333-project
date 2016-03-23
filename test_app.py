bda3e29d5776a7f2c93be69dde81f0c497786a0b
Isabelle Ingato <iingato@princeton.edu>
2016-03-23 12:53:24 -0400

    update courses

--- test_app.py
+++ test_app.py
@@ -104,6 +104,8 @@ def parse_transcript(transcript):
 	#print clas
 	student['courses'] = courses
 	studentinfo = student['name'] + '<br />' + student['degree'] + '<br />' + student['major'] + '<br />'
+	for course in student['courses']:
+		studentinfo = course + '<br />'
 	#curr.execute("INSERT INTO Users VALUES (%s,%s,%s)",(student['name'],student['degree'],student['major']))
 	#conn.commit()
 	#curr.close()
