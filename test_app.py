dbef91879fc97804e491e672fde5ab130e0a8448
Isabelle Ingato <iingato@princeton.edu>
2016-03-23 14:28:41 -0400

    forgot to change port

--- test_app.py
+++ test_app.py
@@ -146,6 +146,6 @@ def upload_file():
 #    return username
 
 if __name__ == "__main__":
-    #port = int(os.environ['PORT'])
-    #app.run(host='0.0.0.0', port=port)
-    app.run(host='127.0.0.1', port=5000)
+    port = int(os.environ['PORT'])
+    app.run(host='0.0.0.0', port=port)
+    #app.run(host='127.0.0.1', port=5000)
