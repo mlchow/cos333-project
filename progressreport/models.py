f5cc41988bf8123d1ed8fec2153f1507bd8dbb53
Isabelle Ingato <iingato@princeton.edu>
2016-04-03 19:44:43 -0400

    test CAS login

--- progressreport/models.py
+++ progressreport/models.py
@@ -1,5 +1,7 @@
 #from views import app
-import psycopg2
+import psycopg2, re
+regex = re.compile('\'|\"')
+#regex2 = re.compile('\( ([^\)]*,) ([^\)]*,) ([^\)]*) \)')
 
 def search_users(netid):
     try:
@@ -45,6 +47,12 @@ def get_progress(netid):
     except:
         return None
     curr = conn.cursor()
+    # if user's progress is stored and does not need to be updated
+    #curr.execute("SELECT fulfilled FROM users WHERE netid = '"+netid+"';")
+    #prog = curr.fetchone()
+    #prog = prog[0]
+    #if prog == None or prog == []:
+        # if user's progress is not already stored or needs to be updated
     curr.execute("SELECT courses FROM users WHERE netid = '"+netid+"';")
     courses = curr.fetchone()
     if courses == None:
@@ -54,7 +62,7 @@ def get_progress(netid):
     courses = courses[0]
     progress = []
     for courseinfo in courses:
-        #print courseinfo
+            #print courseinfo
         course = courseinfo[0]
         grade = courseinfo[1]
         curr.execute("SELECT major_area FROM courses WHERE name = "+course+";")
@@ -62,7 +70,31 @@ def get_progress(netid):
         if major_areas != None:
             major_areas = major_areas[0]
             progress.append((course,grade,major_areas))
+        #save_progress(netid,progress)
     curr.close()
     conn.close()
     return progress
+    #else:
+        #progress = []
+        #for tup in re.finditer(regex2,prog):
+           # print tup.group(1)
+            #print tup.group(2)
+           # print tup.group(3)
+            #progress.append(tup.group(1,2,3))
+       # print progress
+       # return progress
+
+
+def save_progress(netid,progress):
+    try:
+        conn = psycopg2.connect('postgres://gordibbmgwbven:7uBEh3xUMiB5g9c9fpOcXg_Mr9@ec2-54-83-57-25.compute-1.amazonaws.com:5432/d1c29niorsfphk')
+    except:
+        return
+    curr = conn.cursor()
+    progress = str(progress)
+    progress = regex.sub("",progress)
+    curr.execute("UPDATE users SET fulfilled = %s WHERE netid = %s;", (progress,netid))
+    conn.commit()
+    curr.close()
+    conn.close()
 
--- progressreport/views.py
+++ progressreport/views.py
@@ -1,26 +1,27 @@
-import os,psycopg2,urlparse
+import os,psycopg2,urlparse,cgi
 from flask import Flask, request, redirect, url_for
 from flask import render_template
 import CASClient
 from controller import parse_transcript, show_progress
 from models import search_users, add_user, get_progress
+import CASClient
 
 app = Flask(__name__)
 
+form = cgi.FieldStorage()
+
 #from progressreport import app
 
-#C = CASClient.CASClient()
-#os.environ["HTTP_HOST"] = 'progressreport.herokuapp.com'
-#os.environ['REQUEST_URI'] = '/'
+C = CASClient.CASClient()
+os.environ["HTTP_HOST"] = 'progressreport.herokuapp.com'
+os.environ['REQUEST_URI'] = '/welcome.html'
 #netid = "" # bad security but useful for now
 
 @app.route("/")
 def start():
-    #loginpage = C.Authenticate1()
-    #if len(netid) > 15:
-    #return redirect(loginpage)
-    #return '<html><body>' + netid + '</body></html>'
-    return render_template('index.html')
+    loginpage = C.Authenticate1()
+    return redirect(loginpage)
+    #return render_template('index.html')
 
 #@app.route("/",methods=["GET"])
 #def restart():
@@ -30,8 +31,9 @@ def start():
     #    netid = C.Authenticate2(ticket_from_cas)
     #return render_template('templates/index.html',netid)
 
-@app.route("/",methods=["POST"])
+@app.route("/welcome.html",methods=["POST"])
 def upload_file():
+    print C.Authenticate2(form['ticket'].value)
     if request.method == 'POST':
         file = request.files['transcript']
         netid = request.form['netid']
@@ -44,7 +46,7 @@ def upload_file():
                 #return str(ret)
                 #return "<html><body>" + str(get_progress(netid)) + '</body></html>'
                 #return redirect(url_for("success"))
-        return render_template('index.html')
+    return render_template('index.html')
         #str(get_progress(netid)) + '</body></html>'
 
 #@app.route("/see_progress",)
